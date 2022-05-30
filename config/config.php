4e487cc790fae3207c714a4da7f48431eeb426af
Khulood Aldhahri <99732801+Klaudzdeleo45@users.noreply.github.com>
2022-05-30 14:05:35 +0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php 
 
 define("BURL","http://127.0.0.1/project name here/");
-// define("BURLA","http://127.0.0.1/project_restaurant/Admin/"); // if u need admin page
+// define("BURLA","http://127.0.0.1/project_Gym/Admin/"); //if we want to create admin page in the future
 
 define("BL",__DIR__.'/');
-// define("BLA",__DIR__.'/Admin/'); // if u need admin page
+// define("BLA",__DIR__.'/Admin/'); //if we want to create admin page in the future
 require_once(BL.'db.php');
 ?>
